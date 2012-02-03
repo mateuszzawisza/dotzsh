7fe40581c64f523a0d369b9fb0e53c0108bef6d1
Mateusz Zawisza <mateusz@applicake.com>
2012-02-03 11:27:23 +0100

    improves moving by paths

--- config/base.zsh
+++ config/base.zsh
@@ -1,5 +1,9 @@
 autoload colors; colors
 
+autoload -U select-word-style
+WORDCHARS='?_-.[]~=&;!#$%^(){}<>'
+select-word-style normal
+
 # should I load compinit? will this slow things down?
 #autoload -U compinit
 #compinit -i
